784bd78481ceec8268baf127e38a22fffb3dcbce
배재호 <secuho@secuho.life>
2026-02-15 02:23:58 +0900

    2026/02/15

---
+++ java_study/260215/src/Helloworld.java
@@ -0,0 +1,15 @@
+
+public class Helloworld {
+
+    public static void main(String[] args) {
+
+        String first = "hello";
+        String second = "hello";
+
+        System.out.println(first == second);
+        System.out.println(first.equals(second));
+
+    }
+
+
+}
