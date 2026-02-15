93ea2edb5a8548853aa34ae4f545bf4a757be2b4
배재호 <secuho@secuho.life>
2026-02-15 22:45:36 +0900

    2025/02/15

--- python_study/260216.py
+++ python_study/260216.py
@@ -1,5 +1,12 @@
-def sayhello() :
+''' def sayhello() :
     print("안녕하세요 배재호입니다.")
     print("만나서 반갑습니다.")
     
-sayhello()
+sayhello() '''
+
+def func(k) :
+    k = k + 1
+    # print(k)
+    return k
+    
+print(func(6))
