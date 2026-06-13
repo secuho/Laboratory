d41e0f413abeb06245afab52c827eeb3743259c4
배재호 <secuho@secuho.life>
2026-06-14 01:12:16 +0900

    2026/06/01

--- SimP_CSC2002/0601-3.cpp
+++ SimP_CSC2002/0601-3.cpp
@@ -1,22 +1,28 @@
 #include <iostream>
-#include <vector>
+#include <fstream>
 using namespace std;
 
 int main() {
-	vector<int> v;
-	v.push_back(1);
-	v.push_back(2);
-	v.push_back(3);
-
-	vector<int>::iterator it; // iterator는 포인터 참조
-
-	for(it=v.begin(); it != v.end(); it++) {
-		int n = *it;
-		n = n*3;
-		*it = n;
+	// 스트림 객체 생성 및 파일 열기
+	ifstream fin;  
+	fin.open("c:\\temp\\student.txt"); 
+	if(!fin) {  // 파일 열기 실패 확인
+		cout << "파일을 열 수 없다";
+		return 0;
 	}
+	
+	char name[10], dept[20];
+	int sid;
 
-	for(it=v.begin(); it != v.end(); it++)
-		cout << *it << ' ';
-	cout << endl;
+	// 파일 읽기
+	fin >> name; // 파일에 있는 문자열을 읽어서 name 배열에 저장
+	fin >> sid; // 정수를 읽어서 sid 정수형 변수에 저장
+	fin >> dept; // 문자열을 읽고 dept 배열에 저장
+	
+	// 읽은 텍스트를 화면에 출력
+	cout << name << endl;
+	cout << sid << endl;
+	cout << dept << endl;
+
+	fin.close(); // 파일 읽기를 마치고 파일을 닫는다.
 }
